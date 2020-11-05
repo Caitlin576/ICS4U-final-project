03bb34ce10362a6832dd59a75fba35f6bac0bc08
EthanHartley4 <71281918+EthanHartley4@users.noreply.github.com>
2020-11-05 12:48:16 -0500

    Update and rename displayracinggame.py to pygamewindow.py

---
+++ pygamewindow.py
@@ -0,0 +1,19 @@
+import pygame 
+  
+
+pygame.init() 
+
+width = 700
+height = 550
+   
+
+white = (255, 255, 255) 
+red = (255, 0, 0) 
+green = (0, 255, 0) 
+blue = (0, 0, 255) 
+black = (0, 0, 0) 
+
+screen = pygame.display.set_mode((width, height)) 
+  
+
+pygame.display.set_caption("Racing Game") 
