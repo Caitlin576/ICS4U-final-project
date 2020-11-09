c314f12e3f12ff9536ab999e1cedce62b34f1123
Caitlin576 <71281809+Caitlin576@users.noreply.github.com>
2020-11-08 20:45:37 -0500

    Add files via upload

--- pitstop.py
+++ pitstop.py
@@ -5,7 +5,7 @@ class Pitstop(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load("pitstop.png").convert_alpha()
+        self.image = pygame.image.load("pitstopImg.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = 70
         self.rect.y = -10
